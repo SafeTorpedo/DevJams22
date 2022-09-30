581d03b9cea5a0f233bbf985c569aa86e7749875
Pratham Gupta <prathamgupta.pg1@gmail.com>
2022-10-01 00:17:59 +0530

    Added Navbar component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
+import Navbar from "./components/Navbar";
 import Signup from "./components/Signup";
 
 function App() {
     return (
-        <div className="min-h-full h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-            <div className="max-w-md w-full space-y-8">
-                <BrowserRouter>
-                    <Routes>
-                        <Route path="/" element={<Login />} />
-                        <Route path="/signup" element={<Signup />} />
-                    </Routes>
-                </BrowserRouter>
+        <div>
+            <Navbar />
+            <div className="min-h-full h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+                <div className="max-w-md w-full space-y-8">
+                    <BrowserRouter>
+                        <Routes>
+                            <Route path="/" element={<Login />} />
+                            <Route path="/signup" element={<Signup />} />
+                        </Routes>
+                    </BrowserRouter>
+                </div>
             </div>
         </div>
     );
