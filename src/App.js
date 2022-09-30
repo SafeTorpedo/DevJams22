1717c57eee3a930f31024d93c3da304f36be3781
Pratham Gupta <prathamgupta.pg1@gmail.com>
2022-09-30 18:14:28 +0530

    Set up Routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
-    return <div className="text-3xl font-bold">Test</div>;
+    return (
+        <div className="min-h-full h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+            <div className="max-w-md w-full space-y-8">
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" />
+                        <Route path="/signup" />
+                    </Routes>
+                </BrowserRouter>
+            </div>
+        </div>
+    );
 }
 
 export default App;
